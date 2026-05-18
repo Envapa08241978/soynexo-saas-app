9a6d16ddf300774d6b68654d3b46f960237051f2
Enrique Vale <enriquevale.ev@gmail.com>
2026-05-18 12:45:24 -0700

    Switch to brand new Firebase project (soynexo-saas-2026)

--- app/firebase-config.js
+++ app/firebase-config.js
@@ -1,13 +1,12 @@
 // Configuración de Firebase
 // NOTA: Reemplazar con las credenciales de Firebase Console > Project Settings
 const firebaseConfig = {
-  projectId: "soy-nexo",
-  appId: "1:297456603993:web:0f64d149f8ebef16b6f248",
-  storageBucket: "soy-nexo.firebasestorage.app",
-  apiKey: "AIzaSyBOkHtoVXQ12K7P7FYNTB0nvAQW6bAKiTw",
-  authDomain: "soy-nexo.firebaseapp.com",
-  messagingSenderId: "297456603993",
-  measurementId: "G-F22YGRKTM3"
+  projectId: "soynexo-saas-2026",
+  appId: "1:1013348297005:web:ef1299c89f692ac7154ba4",
+  storageBucket: "soynexo-saas-2026.firebasestorage.app",
+  apiKey: "AIzaSyAzz-ILJf3C36GF8KEfQV3kmINqMJpbjyk",
+  authDomain: "soynexo-saas-2026.firebaseapp.com",
+  messagingSenderId: "1013348297005"
 };
 
 // Inicializar Firebase (Solo si no ha sido inicializado)
